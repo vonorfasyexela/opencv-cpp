ed0a66cebcc15661dde80e0809d4a22f966dd73b
Alexey Safronov <vonorfasyexela@gmail.com>
2019-09-13 13:24:16 +0300

    Enhance hello world with 2 types of the version

--- 01-hello-world/hello-world.cpp
+++ 01-hello-world/hello-world.cpp
@@ -1,13 +1,23 @@
 #include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 int main(int argc, char const *argv[])
 {
-    cout << "OpenCV version : " << CV_VERSION << endl;
-    cout << "Major version : " << CV_MAJOR_VERSION << endl;
-    cout << "Minor version : " << CV_MINOR_VERSION << endl;
-    cout << "Subminor version : " << CV_SUBMINOR_VERSION << endl;
- 
+    // getting compile time version
+    cout << "Compile-time" << endl;
+    cout << "\tOpenCV version   : " << CV_VERSION << endl;
+    cout << "\tMajor version    : " << CV_MAJOR_VERSION << endl;
+    cout << "\tMinor version    : " << CV_MINOR_VERSION << endl;
+    cout << "\tSubminor version : " << CV_SUBMINOR_VERSION << endl;
+    
+    // getting run time version
+    cout << "Run-time" << endl;
+    cout << "\tOpenCV version   : " << getVersionString() << endl;
+    cout << "\tMajor version    : " << getVersionMajor() << endl;
+    cout << "\tMinor version    : " << getVersionMinor() << endl;
+    cout << "\tRevision version : " << getVersionRevision() << endl;   
+
     return 0;
 }
