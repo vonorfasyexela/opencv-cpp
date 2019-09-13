42a979825e6b86d74baad727818dece95acb35f3
Alexey Safronov <vonorfasyexela@gmail.com>
2019-09-13 15:51:42 +0300

    Add conditional compilation based on the library version

--- 01-hello-world/hello-world.cpp
+++ 01-hello-world/hello-world.cpp
@@ -6,6 +6,7 @@ using namespace cv;
 int main(int argc, char const *argv[])
 {
     // getting compile time version
+    // the most cross-version method is to use only this "old style" macros
     cout << "Compile-time" << endl;
     cout << "\tOpenCV version   : " << CV_VERSION << endl;
     cout << "\tMajor version    : " << CV_MAJOR_VERSION << endl;
@@ -13,18 +14,20 @@ int main(int argc, char const *argv[])
     cout << "\tSubminor version : " << CV_SUBMINOR_VERSION << endl;
     
     // getting run time version
+    // versions prior to 3.4.2 don't have such functions
+    // that's how we check that the compile-time version is greater then "3.4.2"    
+    cout << "Run-time" << endl;
+#if (CV_MAJOR_VERSION >= 3 && CV_MINOR_VERSION >= 4 && CV_SUBMINOR_VERSION >= 2) || \
+    (CV_MAJOR_VERSION >= 3 && CV_MINOR_VERSION >= 5) || \
+    (CV_MAJOR_VERSION >= 4)
 
-    string version = CV_VERSION;
-    // cout << version.compare("3.4.2") >=  << endl;
-    if (version.compare("3.4.2") >= 0)
-    {
-        cout << "Run-time" << endl;
-        cout << "\tOpenCV version   : " << getVersionString() << endl;
-        cout << "\tMajor version    : " << getVersionMajor() << endl;
-        cout << "\tMinor version    : " << getVersionMinor() << endl;
-        cout << "\tRevision version : " << getVersionRevision() << endl;   
-    }
-
+    cout << "\tOpenCV version   : " << getVersionString() << endl;
+    cout << "\tMajor version    : " << getVersionMajor() << endl;
+    cout << "\tMinor version    : " << getVersionMinor() << endl;
+    cout << "\tRevision version : " << getVersionRevision() << endl;   
+#else
+    cout << "\tNo such functions in this version of library" << endl;
+#endif
 
     return 0;
 }
